2bf6222c4640daa5d27de555ef88e82c6a396105
zengkaiwei2018 <51750122+zengkaiwei2018@users.noreply.github.com>
2019-12-20 11:58:32 +0800

    AACC
    新链表输出

---
+++ 112233.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>//mallocº¯Êý
+using namespace std;
+int s[1000];
+int a[100];
+int main()
+{
+	int n;
+	long long i=0;
+	scanf("%d",&n);
+	while(1)
+	{
+		if(n==-1)
+		{
+			break;
+		}
+		if(n%2==1)
+		{
+			s[i]=n;
+			i++;
+		}
+		scanf("%d",&n);
+	}
+	printf("%d",s[0]);
+	for(i=1;s[i]!=NULL;i++)
+	{
+		printf(" %d",s[i]);
+	}
+	return 0;
+ } 
