2447e3a78eeb928952643da932ad8ffc00383b46
zengkaiwei2018 <51750122+zengkaiwei2018@users.noreply.github.com>
2019-12-22 22:50:04 +0800

    ooooo
    6-1 求二叉树高度 (25分)

---
+++ ooooo.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char ElementType;
+typedef struct TNode *Position;
+typedef Position BinTree;
+struct TNode{
+    ElementType Data;
+    BinTree Left;
+    BinTree Right;
+};
+
+BinTree CreatBinTree(); /* ÊµÏÖÏ¸½ÚºöÂÔ */
+int GetHeight( BinTree BT );
+
+int main()
+{
+    BinTree BT = CreatBinTree();
+    printf("%d\n", GetHeight(BT));
+    return 0;
+}
+int GetHeight( BinTree BT )
+{
+	int HL,HR;
+	if(BT != NULL)
+	{
+		HL=GetHeight(BT->Left);
+		HR=GetHeight(BT->Right);
+			if(HL>=HR)
+				return HL+1;
+		else
+			return HR+1;
+	}
+return 0;
+}
